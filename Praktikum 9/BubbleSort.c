48b8b69fb81a213365267e32521989e1cb0680e2
rafasyaa <100133841+rafasyaa@users.noreply.github.com>
2022-04-28 08:05:19 +0700

    Add files via upload

---
+++ Praktikum 9/BubbleSort.c
@@ -0,0 +1,44 @@
+/*Nama File 	: BubbleSort.c*/
+/*Deskripsi 	: Membuat program mengurutkan integer dengan bubble sort*/
+/*Pembuat   	: Muhammad Rafasya Putra Ristianto - 24060121130074*/
+/*Tgl Pembuatan	: Selasa, 26 April 2022*/
+
+#include <stdio.h>//Header File
+
+//Program Utama
+int main()
+{
+    //Kamus
+    int T[100], N, i;
+    //Algoritma
+    printf("Bubble Sort \n");
+    printf("Masukkan banyak elemen : ");
+    scanf("%d", &N);
+    printf("Masukkan nilai : \n");
+    for(i = 0; i < N; i++){
+        scanf("%d", &T[i]);
+    }
+    //Memanggil Sub Program
+    BubbleSort(T, N);
+    printf("Hasil pengurutan sebagai berikut : \n");
+    for(i = 0; i < N; i++){
+        printf("%d ", T[i]);
+    }
+}
+
+//Sub Program
+void BubbleSort(int T[], int N)
+{
+    //Kamus Lokal
+    int i, k, temp;
+    //Algoritma
+    for(i = 0; i < N; i++){
+        for(k = 0; k < N-i-1; k++){
+            if(T[k] > T[k+1]){
+            temp = T[k];
+            T[k] = T[k+1];
+            T[k+1] = temp;
+            }
+        }
+    }
+}
---
+++ Praktikum 9/InsertionSort.c
@@ -0,0 +1,44 @@
+/*Nama File 	: InsertionSort.c*/
+/*Deskripsi 	: Membuat program mengurutkan integer dengan insertion sort*/
+/*Pembuat   	: Muhammad Rafasya Putra Ristianto - 24060121130074*/
+/*Tgl Pembuatan	: Selasa, 26 April 2022*/
+
+#include<stdio.h>//Header File
+
+//Program Utama
+int main()
+{
+    //Kamus
+    int T[100], N, i;
+    //Algoritma
+    printf("Insertion Sort \n");
+    printf("Masukkan banyak elemen : ");
+    scanf("%d", &N);
+    printf("Masukkan nilai : \n");
+    for(i = 0; i < N; i++){
+        scanf("%d", &T[i]);
+    }
+    //Memanggil Sub Program
+    InsertionSort(T, N);
+    printf("Hasil pengurutan sebagai berikut : \n");
+    for(i = 0; i < N; i++){
+        printf("%d ", T[i]);
+    }
+}
+
+//Sub Program
+void InsertionSort(int T[], int N)
+{
+    //Kamus Lokal
+    int i, temp, j;
+    //Algoritma
+    for (i = 1; i < N; i++) {
+        temp = T[i];
+        j = i - 1;
+        while (j >= 0 && T[j] > temp) {
+            T[j + 1] = T[j];
+            j-- ;
+        }
+        T[j + 1] = temp;
+    }
+}
